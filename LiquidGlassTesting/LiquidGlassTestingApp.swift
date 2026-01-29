c9476fb798208d33a251f52d0e19321f6252d2d0
sebmendo1 <contact@sebastianmendo.design>
2026-01-28 20:07:27 -0600

    Initial Commit

---
+++ LiquidGlassTesting/LiquidGlassTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidGlassTestingApp.swift
+//  LiquidGlassTesting
+//
+//  Created by Sebastian Mendo on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct LiquidGlassTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiquidGlassTestingTests/LiquidGlassTestingTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidGlassTestingTests.swift
+//  LiquidGlassTestingTests
+//
+//  Created by Sebastian Mendo on 1/28/26.
+//
+
+import Testing
+@testable import LiquidGlassTesting
+
+struct LiquidGlassTestingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
